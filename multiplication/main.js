0aeda268adebb8fa31f6663188d12520f9c89df3
code-with-Atomic <ikehchibuikevincent@gmail.com>
2025-08-13 07:50:21 -0700

    add new js

---
+++ multiplication/main.js
@@ -0,0 +1,72 @@
+// multiplication table
+// for (let i = 1 ; i <= 10; i++){
+//     for (let j = 1; j <= 10; j++){
+//         console.log( i + " x " + j + " = " + i * j);
+//     }
+// }
+
+// for(i=1;i<=100;i++){
+//     console.log(i)
+// }
+
+
+
+// function primeNumber(n){
+//     for (let i = 2; i<n; i++){
+
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+//     for ( let i = 2; i<=100; i++){
+//         if (primeNumber(i)){
+//             console.log(i);
+//         }
+//     }
+
+
+// // if 100 is even
+//  const number = 100;
+//  function isEven(number){
+//     if(number % 2 == 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+//  }
+
+//  const myNumber = 100;
+//  const isMyNumberEven = isEven(myNumber)
+//  console.log(isMyNumberEven);
+
+// // filter even and odd numbers
+// const even = function(){
+//     evenNumber = []
+//     let sum = 0
+//     for (let i = 1; i <= 100; i++){
+//         if(i % 2 ===0 ){
+//         evenNumber.push(i);
+//         sum += 1
+//         }
+//     }
+//     console.log(evenNumber);
+//         console.log(sum);
+// }
+// even()
+
+// const odd = function(){
+//     oddNumber = []
+//     let sum = 0
+//     for (let i = 1; i <= 100; i+=2){
+//         oddNumber.push(i)
+//         sum += 1
+//     }
+//     console.log(oddNumber);
+//         console.log(sum);
+// }
+// odd()
+
+
